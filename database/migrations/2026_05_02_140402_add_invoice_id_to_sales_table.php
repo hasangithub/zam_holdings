31213c6a551b497565c14e5320962e26022ac4b4
hasan <hasanbannah@gmail.com>
2026-07-12 21:41:40 +0530

    no message

--- database/migrations/2026_05_02_140402_add_invoice_id_to_sales_table.php
+++ database/migrations/2026_05_02_140402_add_invoice_id_to_sales_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('sales', function (Blueprint $table) {
-            //$table->string('invoice_id')->unique()->after('id');
+            $table->string('invoice_id')->unique()->after('id');
         });
     }
 
