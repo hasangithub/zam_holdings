5698581c7fa234ca4774d03d525d5d9cf2f24dbc
hasan <hasanbannah@gmail.com>
2026-08-21 10:16:55 +0530

    no message

--- resources/views/sales/invoice.blade.php
+++ resources/views/sales/invoice.blade.php
@@ -367,7 +367,7 @@
                 </td>
 
                 <td>
-                    {{ $item->item->code ?? '-' }}
+                    {{ $item->item->item_code ?? '-' }}
                 </td>
 
                 <td>
