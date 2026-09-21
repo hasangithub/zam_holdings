c3745f92806a5daa8e3c48568ba497e1eb2e0b12
hasan <hasanbannah@gmail.com>
2026-09-21 08:36:23 +0530

    no message

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -9,12 +9,7 @@
     <div class="sidebar">
 
         {{-- USER PANEL --}}
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="info">
-                <a href="#" class="d-block">{{ Auth::user()->name ?? 'Admin' }}</a>
-            </div>
-        </div>
-
+      
         {{-- MENU --}}
         <nav class="mt-2">
 
